959ab12141151765e34610ea1b743fd9afaf2c3c
Mohamed Ehab <120674384+Mohamed-Ehab-Elsawy@users.noreply.github.com>
2025-07-22 23:46:05 +0300

    Initial commit.

---
+++ lib/bookly_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+  );
+}
